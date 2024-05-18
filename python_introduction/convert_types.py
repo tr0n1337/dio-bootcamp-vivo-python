a8e8719d9efa8e3ebe9d1a1dde881e32b301a44b
Thiago Fernandes <thiago.souzax3@outlook.com>
2024-05-18 17:49:15 -0300

    refactor: change structure prints

--- python_introduction/convert_types.py
+++ python_introduction/convert_types.py
@@ -1,18 +1,18 @@
 # STR Convert
 NUMBER_TO_STRING = str(10)
-print(NUMBER_TO_STRING)
+print(f"Number to string: {NUMBER_TO_STRING}")
 
 # INT Convert
 # STRING_TO_INT_ERROR = int("a")  # Error
 STRING_TO_INT = int("20")
 FLOAT_TO_INT = int(30.5)
-print(STRING_TO_INT)
-print(FLOAT_TO_INT)
+print(f"String to int: {STRING_TO_INT}")
+print(f"Float to int: {FLOAT_TO_INT}")
 
 # FLOAT Convert
 # STRING_TO_FLOAT_ERROR_1 = float("1")  # Error
 # STRING_TO_FLOAT_ERROR_2 = float("a")  # Error
 STRING_TO_FLOAT = float("40.2")
 INT_TO_FLOAT = float(50)
-print(STRING_TO_FLOAT)
-print(INT_TO_FLOAT)
+print(f"String to float: {STRING_TO_FLOAT}")
+print(f"Int to float: {INT_TO_FLOAT}")
